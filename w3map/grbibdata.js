e0ae5d937203fe180a8c09c004065c5799a037b0
mhaveyzafin <michael.havey@zafin.com>
2016-12-13 14:58:03 -0500

    true up

---
+++ w3map/grbibdata.js
@@ -0,0 +1,388 @@
+/*
+Source: Wikipedia for MOST of IT, including geo coordinates, dates.
+http://www.matthewmcgee.org/paultime.html - paul data
+*/
+
+var dataset = [
+		{
+			"Tradition" : "GR",
+			"Author" : "Thales",
+			"StartYear" : -624,
+			"EndYear" : -546,
+			"Location" : "Miletus",
+			"Latitude" : 37.530233,
+			"Longitude" : 27.278369,
+			"Tags" : "presocratic, water"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Anaximander",
+			"StartYear" : -610,
+			"EndYear" : -546,
+			"Location" : "Miletus",
+			"Latitude" : 37.530233,
+			"Longitude" : 27.278369,
+			"Tags" : "presocratic, apeiron"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Pythagoras",
+			"StartYear" : -570,
+			"EndYear" : -495,
+			"Location" : "Samos",
+			"Latitude" : 37.75479,
+			"Longitude" : 26.97777,
+			"Tags" : "presocratic, school, no beans, pythagorean theorem, coined term philosophy,  metempsychosis, soul of dead reborn in another body"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Anaximenes",
+			"StartYear" : -585,
+			"EndYear" : -528,
+			"Location" : "Miletus",
+			"Latitude" : 37.530233,
+			"Longitude" : 27.278369,
+			"Tags" : "presocratic, air"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Xenophanes",
+			"StartYear" : -570,
+			"EndYear" : -475,
+			"Location" : "Colophon",
+			"Latitude" : 38.108889,
+			"Longitude" : 27.141667,
+			"Tags" : "presocratic, incorporeal god, scepticism"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Parmenides",
+			"StartYear" : -515,
+			"EndYear" : -445,
+			"Location" : "Elea",
+			"Latitude" : 40.160833,
+			"Longitude" : 15.155,
+			"Tags" : "presocratic, school, reason over the senses, being is eternal, nothing comes into being, no motion, ontology"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Heraclitus",
+			"StartYear" : -535,
+			"EndYear" : -475,
+			"Location" : "Ephesus",
+			"Latitude" : 37.941111,
+			"Longitude" : 27.341944,
+			"Tags" : "presocratic, flux, fire, cant step into the same river twice, logos, character is fate, daimon"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Anaxagoras",
+			"StartYear" : -510,
+			"EndYear" : -428,
+			"Location" : "Clazomenae",
+			"Latitude" : 38.358167,
+			"Longitude" : 26.767583,
+			"Tags" : "presocratic, mind, nous, nature was unformed until mind formed it"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Empedocles",
+			"StartYear" : -490,
+			"EndYear" : -430,
+			"Location" : "Acragas",
+			"Latitude" : 37.316667,
+			"Longitude" : 13.583333,
+			"Tags" : "presocratic, earth, air, water, fire, love attracts them strife divides them, reincarnation"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Zeno",
+			"StartYear" : -490,
+			"EndYear" : -430,
+			"Location" : "Elea",
+			"Latitude" : 40.160833,
+			"Longitude" : 15.155,
+			"Tags" : "presocratic, paradox, reductio ad absurdum, motion is an illusion"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Leucippus",
+			"StartYear" : -490,
+			"EndYear" : -430,
+			"Location" : "Miletus",
+			"Latitude" : 37.530233,
+			"Longitude" : 27.278369,
+			"Tags" : "presocratic, atomism, logos, everything happens from necessity"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Protagoras",
+			"StartYear" : -490,
+			"EndYear" : -420,
+			"Location" : "Abdera",
+			"Latitude" : 40.933333,
+			"Longitude" : 24.966667,
+			"Tags" : "presocratic, anti-math, sophist, use of words, orthopeia, rhetoric, relativism, man is the measure of all things, agnosticism"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Philolaus",
+			"StartYear" : -470,
+			"EndYear" : -385,
+			"Location" : "Croton",
+			"Latitude" : 39.083333,
+			"Longitude" : 17.116667,
+			"Tags" : "presocratic, limits, harmonia, harmony of limited and unlimited, astronomy, central fire"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Democritus",
+			"StartYear" : -460,
+			"EndYear" : -370,
+			"Location" : "Abdera",
+			"Latitude" : 40.933333,
+			"Longitude" : 24.966667,
+			"Tags" : "presocratic, atomism, bastard vs legitimate knowledge"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Xenophon",
+			"StartYear" : -430,
+			"EndYear" : -354,
+			"Location" : "Athens",
+			"Latitude" : 37.966667,
+			"Longitude" : 23.716667,
+			"Tags" : "presocratic, socratic dialogues"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Socrates",
+			"StartYear" : -469,
+			"EndYear" : -399,
+			"Location" : "Athens",
+			"Latitude" : 37.966667,
+			"Longitude" : 23.716667,
+			"Tags" : "big3"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Plato",
+			"StartYear" : -427,
+			"EndYear" : -347,
+			"Location" : "Athens",
+			"Latitude" : 37.966667,
+			"Longitude" : 23.716667,
+			"Tags" : "big3"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Aristotle",
+			"StartYear" : -384,
+			"EndYear" : -322,
+			"Location" : "Stagira",
+			"Latitude" : 40.591667,
+			"Longitude" : 23.794722,
+			"Tags" : "big3"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Diogenes",
+			"StartYear" : -412,
+			"EndYear" : -323,
+			"Location" : "Sinope",
+			"Latitude" : 42.033333,
+			"Longitude" : 35.15,
+			"Tags" : "hellenistic, cynic, return to simplicity of nature, austerity, cosmopolitanism, citizen of the world, dog-like behavior, hoarding, indifference to life, self-sufficiency, opposed to conventional thinking"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Epicurus",
+			"StartYear" : -341,
+			"EndYear" : -271,
+			"Location" : "Samos",
+			"Latitude" : 37.75479,
+			"Longitude" : 26.97777,
+			"Tags" : "hellenistic, epicurean, pleasure and pain, good and evil, strive to be free of suffering, tranquility, ataraxia, avoidance of overindulgence,  problem of evil paradox, principle of multiple explanations, "
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Pyrrho",
+			"StartYear" : -360,
+			"EndYear" : -270,
+			"Location" : "Elis",
+			"Latitude" : 37.720923,
+			"Longitude" : 21.607014,
+			"Tags" : "hellenistic, scpeticism"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Zeno",
+			"StartYear" : -334,
+			"EndYear" : -262,
+			"Location" : "Citium",
+			"Latitude" : 34.9233,
+			"Longitude" : 33.6305,
+			"Tags" : "hellenistic, stoic, fire, the universe accomplishes what is right and prevents the opposite, live according to the reason of the universe, self-preservation"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Cleanthes",
+			"StartYear" : -330,
+			"EndYear" : -230,
+			"Location" : "Assos",
+			"Latitude" : 39.487778,
+			"Longitude" : 26.336944,
+			"Tags" : "hellenistic, stoic, pantheism, soul is material substance, souls live after death, avoid the passions, live consistently"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Chrysippus",
+			"StartYear" : -280,
+			"EndYear" : -207,
+			"Location" : "Soli",
+			"Latitude" : 36.741944,
+			"Longitude" : 34.54,
+			"Tags" : "hellenistic, stoic, logic, syllogism, knowledge is empircal, all things happen by fate, everything is caused, acts are determined but how we respond is a moral choice, compatibilism"
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Cicero",
+			"StartYear" : -106,
+			"EndYear" : -43,
+			"Location" : "Arpino",
+			"Latitude" : 41.647778,
+			"Longitude" : 13.609722,
+			"Tags" : "hellenistic, stoic, "
+		},
+		{
+			"Tradition" : "GR",
+			"Author" : "Epictetus",
+			"StartYear" : 55,
+			"EndYear" : 135,
+			"Location" : "Hierapolis",
+			"Latitude" : 37.925,
+			"Longitude" : 29.125833,
+			"Tags" : "hellenistic, stoic, self-knowledge, practice over logic, we have no power over external things, do not be troubled by loss"
+		}, {
+			"Tradition" : "GR",
+			"Author" : "Marcus Aurelius",
+			"StartYear" : 121,
+			"EndYear" : 180,
+			"Location" : "Rome",
+			"Latitude" : 41.9,
+			"Longitude" : 12.5,
+			"Tags" : "hellenistic, philosopher king, stoic, "
+		}, {
+			"Tradition" : "GR",
+			"Author" : "Plotinus",
+			"StartYear" : 204,
+			"EndYear" : 270,
+			"Location" : "Lycopolis",
+			"Latitude" : 31.083333,
+			"Longitude" : 30.95,
+			"Tags" : "hellenistic, neo-platonic, the one, "
+		}, {
+			"Tradition" : "GR",
+			"Author" : "Augustine",
+			"StartYear" : 354,
+			"EndYear" : 430,
+			"Location" : "Hippo",
+			"Latitude" : 36.9,
+			"Longitude" : 7.766667,
+			"Tags" : "hellenistic, christian, catholic"
+		}, {
+			"Tradition" : "GR",
+			"Author" : "Hypatia",
+			"StartYear" : 370,
+			"EndYear" : 415,
+			"Location" : "Alexandria",
+			"Latitude" : 31.2,
+			"Longitude" : 29.916667,
+			"Tags" : "hellenistic, female, math"
+		}, {
+			"Tradition" : "GR",
+			"Author" : "Boethius",
+			"StartYear" : 480,
+			"EndYear" : 524,
+			"Location" : "Rome",
+			"Latitude" : 41.9,
+			"Longitude" : 12.5,
+			"Tags" : "hellenistic, neo-platonic, martyr, last of the romans"
+		} ,
+		{
+			"Tradition" : "B",
+			"Author" : "Amos",
+			"StartYear" : -765,
+			"EndYear" : -760,
+			"Location" : "Tekoa",
+			"Latitude" : 31.653017,
+			"Longitude" : 35.229033,
+			"Tags" : "old testament, minor prophet, jeroboam II, uzziah, destructive power of God, ancient hymn, locusts, fruit, day of the lord, praise of farmers, contempt for the wealthy"
+		} ,
+		{
+			"Tradition" : "E",
+			"Author" : "Paul in Damascus",
+			"StartYear" : 37,
+			"EndYear" : 40,
+			"Location" : "Damascus",
+			"Latitude" : 33.513056,
+			"Longitude" : 36.291944,
+			"Tags" : "new testament, event, paul"
+		} ,
+		{
+			"Tradition" : "E",
+			"Author" : "Crucifixion of Jesus",
+			"StartYear" : 32,
+			"EndYear" : 32,
+			"Location" : "Jerusalem",
+			"Latitude" : 31.783333,
+			"Longitude" : 35.216667,
+			"Tags" : "new testament, crucifixion, resurrection"
+		} ,
+		{
+			"Tradition" : "E",
+			"Author" : "Paul in Damascus",
+			"StartYear" : 37,
+			"EndYear" : 40,
+			"Location" : "Damascus",
+			"Latitude" : 33.513056,
+			"Longitude" : 36.291944,
+			"Tags" : "new testament, event, paul, galatians, acts, king aretas, stoning of stephen"
+		} 
+		
+		/*
+		{
+			"Tradition" : "B",
+			"Author" : "Isaiah 1 ",
+			"StartYear" : -765,
+			"EndYear" : -760,
+			"Location" : "Tekoa", 
+			"Latitude" : 31.653017, ??
+			"Longitude" : 35.229033, ??
+			"Tags" : "old testament, prophet, proto isaiah, deutero isaih, trito isaiha, Isaiah ben Amoz"
+		} */
+		];
+
+// The three major centers. Want to display them
+var mainCenters = [ {
+	title : 'Jersusalem',
+	pos : {
+		lat : 31.783333,
+		lng : 35.216667
+	}
+}, {
+	title : 'Athens',
+	pos : {
+		lat : 37.966667,
+		lng : 23.716667
+	}
+}, {
+	title : 'Rome',
+	pos : {
+		lat : 41.9,
+		lng : 12.5
+	}
+} ];
+
+// We will center the map on Athens
+var centerPos = mainCenters[1].pos;
